afbdbb6f9fa8f7a6a55a034c90ac72e4adf886b0
yuwan <24859419@qq.com>
2021-11-26 18:19:33 +0800

    先注释文件

--- demo.py
+++ demo.py
@@ -9,10 +9,10 @@
 ------------ ------- -------- -----------
 2021/11/19 5:09 下午 yuwan  1.0  None
 """
-import requests
+# import requests
 
 if __name__ == '__main__':
     print('使用pycharm链接demo进行开发')
     print('测试步骤')
-    response = requests.get('https://www.baidu.com')
-    print(response.text)
+    # response = requests.get('https://www.baidu.com')
+    # print(response.text)
