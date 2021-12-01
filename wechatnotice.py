2501301da7d41c74619f868ef70e25f951ac27ec
yuwan <24859419@qq.com>
2021-12-01 11:06:47 +0800

    增加用户名密码

--- wechatnotice.py
+++ wechatnotice.py
@@ -28,8 +28,9 @@ class InformRobot:
             "msgtype": "markdown",  # 消息类型，此时固定为markdown
             "markdown": {
                 "content": f"任务{jobname}构建开始！ \n" +
-                           f'查看链接: [这是一个链接]({job_url})。 \n' +
-                           f"负责人：@ {noticeperson}  \n"
+                           f'查看链接: [{{job_url}}]({job_url})。 \n' +
+                           f"负责人：@ {noticeperson}  \n" +
+                           f'用户名：test01， 密码：test01'
             }
         }
 
