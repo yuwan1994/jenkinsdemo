6c422fa5e2f6fbc47036fb0a92949f0d6261ffa2
yuwan <24859419@qq.com>
2021-12-01 10:01:34 +0800

    微信通知

---
+++ wechatnotice.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File : wechatnotice.py
+@Contact : woshiyuwan@gmail.com
+@License : (C)Copyright 2020-2021
+@introduction : 实现企业微信通知
+@Modify Time @Author @Version @Description
+------------ ------- -------- -----------
+2021/12/1 9:52 上午 yuwan  1.0  None
+"""
+import requests
+import json
+import urllib3
+# import time
+import sys
+from GetCaseTitle import GetCaseTitle
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class InformRobot:
+    def __init__(self, webhook):  # webhook地址
+        self.url = webhook
+        self.sess = requests.session()
+
+    def markdown_robot(self, noticeperson):
+        data = {
+            "msgtype": "markdown",  # 消息类型，此时固定为markdown
+            "markdown": {
+                "content": "构建开始！ \n" +
+                           f"负责人：@{noticeperson}  \n"
+            }
+        }
+
+        re_post = self.sess.post(self.url, data=json.dumps(data), verify=False)
+        print(re_post.content, data)
+
+
+if __name__ == "__main__":
+    webhook = sys.argv[1]
+    noticeperson = sys.argv[2]
+    # execute_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    InformRobot(webhook).markdown_robot(noticeperson)
